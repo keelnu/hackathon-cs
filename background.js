68d1ca0c37b7673e89c1e47ffed27fe5056312f2
Jennifer Courtner <jmichele.courtner@gmail.com>
2020-10-28 19:35:07 -0400

    updated files

--- background.js
+++ background.js
@@ -4,8 +4,23 @@
 
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({ color: '#3aa757' }, function () {
+    console.log('The color is green.');
   });
 });
+
+// chrome.runtime.onInstalled.addListener(() => {
+//   console.log('onInstalled...');
+//   // create alarm after extension is installed / upgraded
+//   chrome.alarms.create('refresh', { periodInMinutes: 3 });
+// });
+
+// chrome.alarms.onAlarm.addListener((alarm) => {
+//   console.log(alarm.name); // refresh
+//   helloWorld();
+// });
+
+// function helloWorld() {
+//   console.log("Hello, world!");
+// }
